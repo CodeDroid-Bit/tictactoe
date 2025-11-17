62da63b6725518ad064308c6f7d0ba931fd3124f
CodeDroid-Bit <jakedavis@phoenixschool.org>
2025-11-17 16:08:14 -0500

    commit

--- tictactoe.py
+++ tictactoe.py
@@ -11,22 +11,36 @@ BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 # x, y, width, height
-rect1 = pygame.Rect(0, 400, 600, 15)
-rect2 = pygame.Rect(0, 175, 600, 15)
-rect3 = pygame.Rect(175, 0, 15, 600)
-rect4 = pygame.Rect(400, 0, 15, 600)
+line1 = pygame.Rect(0, SCREEN_HEIGHT/3, SCREEN_HEIGHT, 15) #top sideways
+line2 = pygame.Rect(0, SCREEN_HEIGHT/3*2, SCREEN_HEIGHT, 15) #bottom sideways
+line3 = pygame.Rect(SCREEN_HEIGHT/3, 0, 15, SCREEN_HEIGHT) #
+line4 = pygame.Rect(SCREEN_HEIGHT/3*2, 0, 15, SCREEN_HEIGHT)
+Shape_x = pygame.rect()
+
+def tictactoe_board(line1,line2,line3,line4):
+    pygame.draw.rect(screen, BLACK, line1)
+    pygame.draw.rect(screen, BLACK, line2)
+    pygame.draw.rect(screen, BLACK, line3)
+    pygame.draw.rect(screen, BLACK, line4)
+    return
+
+
+screen.fill((255, 255, 255))
+
+
 
 running = True
 while running:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			running = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_pos = event.pos
 
-	screen.fill((255, 255, 255))
-	pygame.draw.rect(screen, BLACK, rect1)
-	pygame.draw.rect(screen, BLACK, rect2)
-	pygame.draw.rect(screen, BLACK, rect3)
-	pygame.draw.rect(screen, BLACK, rect4)
+            if area_1.collidepoint(mouse_pos):
+                pygame.draw.rect(screen, BLACK, Shape_x)
 
-	pygame.display.flip()
+    tictactoe_board(line1,line2,line3,line4)
+
+    pygame.display.flip()
 pygame.quit()
